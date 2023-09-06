872c45052b51b635cbcc756740e3d20414a24979
yisus37 <jahernandez@exikhan.com.mx>
2023-09-06 11:06:25 -0600

    first commit

---
+++ src/Template/Styles/estilosGeneral.ts
@@ -0,0 +1,9 @@
+import { SxProps, Theme } from "@mui/material";
+
+export const flexRow: SxProps<Theme>={display:"flex",justifyContent:"center",alignItems:"center",width:"100%",height:"100%",flexDirection:"row"}
+export const flexColumn: SxProps<Theme>={display:"flex",justifyContent:"center",alignItems:"center",width:"100%",height:"100%",flexDirection:"column"}
+
+/*
+chat #66b2ff
+
+*/
